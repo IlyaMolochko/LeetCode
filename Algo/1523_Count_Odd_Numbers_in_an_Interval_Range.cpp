ec2469b9dbb1518ea777c5002c247fc4f1bedc56
IlyaMolochko <ilja.molochko97@gmail.com>
2023-02-13 09:23:23 +0300

    1523

---
+++ Algo/1523_Count_Odd_Numbers_in_an_Interval_Range.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        return ((high - low + 1) % 2 == 0 ? (high - low + 1) / 2 : (low % 2 ? (high - low + 1) / 2 + 1 : (high - low + 1) / 2));
+    }
+};
