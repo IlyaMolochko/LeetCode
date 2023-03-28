08e500f4f2f7ae76091c5c9f5a596f213ad3631d
IlyaMolochko <ilja.molochko97@gmail.com>
2023-03-28 22:43:46 +0300

    983

---
+++ Algo/983_Minimum_Cost_For_Tickets.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        n = days[-1] + 1
+        dp = [0] * n
+        days = set(days)
+        for i in range(1, n):
+            if i in days:
+                dp[i] = min(dp[max(i - 1, 0)] + costs[0], dp[max(i - 7, 0) ] + costs[1], dp[max(i - 30, 0)] + costs[2])
+            else:
+                dp[i] = dp[i - 1]
+        return dp[-1]
+
