fbaa00c6a4530bb0d5f0eb7d89ef49f221a9c2c1
Malochka Ilya <ilja.molochko97@gmail.com>
2024-04-08 14:42:46 +0300

    Update 1700. Number of Students Unable to Eat Lunch.py

--- Algo/1700. Number of Students Unable to Eat Lunch.py
+++ Algo/1700. Number of Students Unable to Eat Lunch.py
@@ -11,7 +11,7 @@ class Solution:
             if s == 0 and cnt1 == 0:
                 return cnt2
             if s == 1 and cnt2 == 0:
-                return cnt2
+                return cnt1
             if s == 0:
                 cnt1 -= 1
             else:
