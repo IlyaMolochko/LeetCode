9941de3566b955b1ed2f93c1e776c9bd72c586fe
Malochka Ilya <ilja.molochko97@gmail.com>
2024-04-08 13:30:01 +0300

    1700

---
+++ Algo/1700. Number of Students Unable to Eat Lunch.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        cnt1 = 0
+        cnt2 = 0
+        for s in students:
+            if s == 0:
+                cnt1 += 1
+            else:
+                cnt2 += 1
+        for s in sandwiches:
+            if s == 0 and cnt1 == 0:
+                return cnt2
+            if s == 1 and cnt2 == 0:
+                return cnt2
+            if s == 0:
+                cnt1 -= 1
+            else:
+                cnt2 -= 1
+        return 0
